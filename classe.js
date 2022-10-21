afac15618314c8160eeb7eeb95807817038bfed1
anthony <anthony.rabot@gmail.com>
2022-10-21 15:32:17 +0200

    First commit

---
+++ classe.js
@@ -0,0 +1,12 @@
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+}
+
+let livre1 = new Book('livre 1', 'Anthony', 45);
+
+console.log(livre1);
+console.log('test');
